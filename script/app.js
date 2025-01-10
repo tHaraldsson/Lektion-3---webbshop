cfce3b9847358f443be106f82587fd236e0c41b6
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-01-10 10:15:50 +0100

    fifth

---
+++ script/app.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("myButton");
+const outputDiv = document.getElementById("output");
+
+button.addEventListener('click', () => {
+    const text = document.createElement("p");
+    text.textContent = "Du har tryck på knappen!";
+
+    const image = document.createElement("img");
+    image.src = "https://xxxbunker.com/amateur_midget_fucks_hot_shemale-1.jpg";
+
+    outputDiv.innerHTML = "";
+
+    outputDiv.appendChild(text);
+    outputDiv.appendChild(image);
+});
